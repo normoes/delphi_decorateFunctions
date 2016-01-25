3a998294b7727e8286c73bc64271a7f45e228934
Norman <norman.moeschterschenck@de.festo.com>
2016-01-25 08:04:35 +0100

    running order modified

--- Unit1.dfm
+++ Unit1.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 300
-  ClientWidth = 635
+  ClientHeight = 342
+  ClientWidth = 825
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,15 +11,34 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object Button1: TButton
     Left = 120
-    Top = 64
-    Width = 75
-    Height = 25
-    Caption = 'Button1'
+    Top = 24
+    Width = 217
+    Height = 58
+    Caption = 'decorated with ShowMessage'
     TabOrder = 0
     OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 120
+    Top = 104
+    Width = 217
+    Height = 57
+    Caption = 'decorated with logging outputs'
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Memo1: TMemo
+    Left = 400
+    Top = 24
+    Width = 401
+    Height = 289
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 2
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -10,22 +10,38 @@ uses
 type
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Memo1: TMemo;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
+// function's type definition
+// 1. example: simply decorating with ShowMessage(...)
   TMyFunc_Ref = reference to procedure();
-  TMyFunc_Ref1 = reference to procedure(aValue : String);
+// 2. example: passing a parameter
+  TMyFunc_Ref1 = reference to procedure(aValue : String; aMemo : TMemo);
 
-function sayHello_Decorator_Who(aMyFunc_Ref : TMyFunc_Ref):TMyFunc_Ref;
-function sayHello_Decorator_When(aMyFunc_Ref : TMyFunc_Ref):TMyFunc_Ref;
-function sayHello_Decorator1(aMyFunc_Ref : TMyFunc_Ref1):TMyFunc_Ref1;
-function sayHello_Decorator2(aMyFunc_Ref : TMyFunc_Ref1):TMyFunc_Ref1;
-procedure sayHello();
-procedure sayHello1(aValue : String);
+//functions to be decorated
+// 1. example: simply decorating with ShowMessage(...)
+  procedure sayHello();
+// 2. example: passing a parameter
+  procedure sayHello1(aValue : String; aMemo : TMemo);
+
+// anonymous method is defined as Result of the function
+// --> the function returned is the decorated original function
+// (--> can be decoreated n times)
+// 1. example
+  function sayHello_Decorator_Who(aMyFunc_Ref : TMyFunc_Ref):TMyFunc_Ref;
+  function sayHello_Decorator_When(aMyFunc_Ref : TMyFunc_Ref):TMyFunc_Ref;
+// 2. example
+  function sayHello_Decorator1(aMyFunc_Ref : TMyFunc_Ref1):TMyFunc_Ref1;
+  function sayHello_Decorator2(aMyFunc_Ref : TMyFunc_Ref1):TMyFunc_Ref1;
 
 var
   Form1: TForm1;
@@ -34,23 +50,29 @@ implementation
 
 {$R *.dfm}
 
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Memo1.Clear;
+end;
+
+// 1. example: simply decorating with ShowMessage(...)
 procedure TForm1.Button1Click(Sender: TObject);
 var
   aFunc : TMyFunc_Ref;
-  aFunc1 : TMyFunc_Ref1;
 begin
+  Memo1.Clear;
 //  sayHello();
   aFunc := nil;
   aFunc := sayHello_Decorator_Who(sayHello);
 //  aFunc();
   aFunc := sayHello_Decorator_When(aFunc);
+  // calling the double decorated procedure
   aFunc();
+end;
 
-  aFunc1 := nil;
-  aFunc1 := sayHello_Decorator1(sayHello1);
-//  aFunc1('1 --> ');
-  aFunc1 := sayHello_Decorator2(aFunc1);
-  aFunc1('2 --> ');
+procedure sayHello();
+begin
+  ShowMessage('hello');
 end;
 
 function sayHello_Decorator_Who(aMyFunc_Ref : TMyFunc_Ref):TMyFunc_Ref;
@@ -58,7 +80,7 @@ begin
   Result := procedure()
     begin
       aMyFunc_Ref();
-      showMessage('out there');
+      showMessage('in decorator added at this beginning:'+sLinebreak+' ''out there''');
     end;
 end;
 
@@ -67,35 +89,71 @@ begin
   Result := procedure()
     begin
       aMyFunc_Ref();
-      showMessage('at this beautiful time of teh day');
+      showMessage('in decorator added at the end:'+sLinebreak+' ''at this beautiful time of this day''');
     end;
 end;
 
-procedure sayHello();
+// 2. example: passing a parameter
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  aFunc : TMyFunc_Ref1;
 begin
-  ShowMessage('hello');
+  Memo1.Clear;
+  sayHello1('calling the procedure itself', Memo1);
+  Memo1.Lines.Add('------------');
+  aFunc := nil;
+  aFunc := sayHello_Decorator1(sayHello1);
+//  aFunc('1 --> ');
+  aFunc := sayHello_Decorator2(aFunc);
+  // calling the double decorated procedure
+  aFunc('Hello', Memo1);
 end;
 
-
-// overlaoded versions --> generic versions
-procedure sayHello1(aValue : String);
+procedure sayHello1(aValue : String; aMemo : TMemo);
 begin
-  ShowMessage(aValue);
+  if ( Assigned(aMemo) ) then
+  begin
+    aMemo.Lines.add(sLineBreak);
+    aMemo.Lines.add(aValue);
+    aMemo.Lines.add(sLineBreak);
+  end
+  else
+    ShowMessage('no graphical component for output:'+sLineBreak+aValue);
 end;
 
 function sayHello_Decorator1(aMyFunc_Ref : TMyFunc_Ref1):TMyFunc_Ref1;
 begin
-  Result := procedure(aValue : String = '')
+  Result := procedure(aValue : String = ''; aMemo : TMemo = nil)
     begin
-      aMyFunc_Ref(aValue + ' out there');
+      if ( Assigned(aMemo) ) then
+      begin
+        aMemo.Lines.Add('  --> logging: in decorator added at the beginning');
+        aMemo.Lines.Add('  -->          calling the decorated procedure with ''at this beautiful time of the day''');
+      end;
+      aMyFunc_Ref(aValue + ' ''at this beautiful time of the day''', aMemo);
+      if ( Assigned(aMemo) ) then
+      begin
+        aMemo.Lines.Add('  --> logging: in decorator added at the beginning');
+        aMemo.Lines.Add('  -->          after calling the procedure');
+      end;
     end;
 end;
 
 function sayHello_Decorator2(aMyFunc_Ref : TMyFunc_Ref1):TMyFunc_Ref1;
 begin
-  Result := procedure(aValue : String = '')
+  Result := procedure(aValue : String = ''; aMemo : TMemo = nil)
     begin
-      aMyFunc_Ref(aValue + ' at this beautiful time of the day');
+      if ( Assigned(aMemo) ) then
+      begin
+        aMemo.Lines.Add('  --> logging: in decorator added at the end');
+        aMemo.Lines.Add('  -->          calling the decorated procedure with ''out there''');
+      end;
+      aMyFunc_Ref(aValue + ' ''out there''', aMemo);
+      if ( Assigned(aMemo) ) then
+      begin
+        aMemo.Lines.Add('  --> logging: in decorator added at the end');
+        aMemo.Lines.Add('  -->          after calling the procedure');
+      end;
     end;
 end;
 
